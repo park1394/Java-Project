90e8541227090e5bff7bc03e8d0479e9c421c8fd
park1394 <qkrwnl0330@gmail.com>
2026-01-14 10:44:47 +0900

    .

---
+++ java_hello_world/src/_1_test_260113/_0_HELLOWORLD.java
@@ -0,0 +1,10 @@
+package _1_test_260113;
+
+import com.sun.tools.javac.Main;
+
+public class _0_HELLOWORLD {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("오늘 점심 뭐 먹지?");
+    }
+}
